e874031d59a427cfe04e76a41674a8b56dca2e9f
Oleh Levchenko <leva13007@gmail.com>
2025-06-03 01:41:07 +0100

    fix: add rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named'
+    }
+  ],
+  plugins: [nodeResolve(), typescript()],
+  external: []
+};
